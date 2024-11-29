271eadc1836ec1cd853231d00bd081bc52dc944c
sejin1048 <hsjin1048@gmail.com>
2024-11-29 13:52:27 +0900

    python1

---
+++ 1.py
@@ -0,0 +1,22 @@
+'''
+print("hello world!")
+print("hello", "Python", 1, 2, "ASDF", sep="\n")
+print()
+print("1")
+
+print("안녕하세요",end="")
+print("코딩온입니다.")
+
+name ='홍길동'
+age = 30
+print(f"나의 이름은 {name}입니다. 나이는 {age}입니다.")
+'''
+ive = "I AM"
+print(ive)
+ive = "장원영"
+IVE = "카리나"
+print(ive,IVE)
+
+# my_home : snake_case
+# MyHome : CalmelCase <-클래스이름적을때 사용
+# myHome : PascalCase
