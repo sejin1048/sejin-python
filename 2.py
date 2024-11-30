0a1dcda1e35696b103cb937cd1dcdb82829a7102
sejin1048 <hsjin1048@gmail.com>
2024-11-30 13:53:29 +0900

    second

---
+++ 2.py
@@ -0,0 +1,26 @@
+print(3//2)
+print(3.25//2)
+print(3%2)
+print(3.25%2)
+print(2**3)
+print(2**10)
+
+print(1+2*(3**2))
+print(1+2*3**2)
+print(1+2*(-3)**2)
+print(1+2*-3**2)
+
+
+bread=30
+people=4
+print(f"빵의 갯수 : {bread//people}")
+print(f"남은 빵의 갯수 : {bread%people}")
+
+a=1
+a+=1
+print(a)
+
+print("장원영"+" 럭키비키")
+print("럭키"*10)
+#print("럭키"*"비키")#에러
+
