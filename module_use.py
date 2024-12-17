1d2f9623233f3717c5619d36573a6888c4fb0e0e
sejin1048 <hsjin1048@gmail.com>
2024-12-17 09:04:29 +0900

    module_use

---
+++ module_use.py
@@ -0,0 +1,17 @@
+# from modules.mylib import food
+
+# print(food.name)
+# food.cook()
+# food.eat()
+
+# from modules.mylib.food import cook, eat, name
+
+# print(name)
+# cook()
+# eat()
+
+# import modules.mylib.food
+# modules.mylib.food.cook()
+
+# import bbb.cm2 as bc
+# print(bc.add(1,2))
